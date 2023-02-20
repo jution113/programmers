89dd1bf53313c78d903b3cee50f7d2d13e80de9b
SungCheon Ju <53260199+jution113@users.noreply.github.com>
2023-02-20 15:48:13 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.02 ms, Memory: 65 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] solution(int money) {
+        int[] answer = new int[2];
+        final int coffeePrice = 5500;
+        
+        answer[0] = money / coffeePrice;
+        answer[1] = money % coffeePrice;
+        
+        return answer;
+    }
+}
