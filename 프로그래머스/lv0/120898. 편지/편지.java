1aafa448f7b97638e1da4349293d18fc68f043ab
SungCheon Ju <53260199+jution113@users.noreply.github.com>
2023-02-25 13:54:23 +0900

    [level 0] Title: 편지, Time: 0.02 ms, Memory: 71.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120898. 편지/편지.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(String message) {
+        int answer = 0;
+        final int width = 2; 
+        int length = message.length();
+        
+        answer = width * length;
+            
+        return answer;
+    }
+}
